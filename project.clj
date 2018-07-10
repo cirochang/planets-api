479881fe34cb84306c1a8c30c5a9e7c9ad5ad9a2
Ciro Chang <ciro.chang@tvglobo.com.br>
2018-07-09 21:40:00 -0300

    add an basic project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject job-queues "1.0.0"
+  :description "An exercise of planets API provided by B2W."
+  :url "https://gitlab.com/cirochang/planets-api"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :target-path "target/%s"
+)
