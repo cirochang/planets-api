0cf74c44232048bc5291fb9fdbc2208a005b585f
Ciro Chang <ciro.chang@tvglobo.com.br>
2018-07-10 01:11:52 -0300

    add database manager

---
+++ src/planets_api/database.clj
@@ -0,0 +1,34 @@
+(ns planets-api.database
+  (:require monger.json
+            [monger.db :refer [drop-db]]
+            [monger.core :as mg]
+            [monger.collection :as mc]
+            [monger.query :as query]
+            [monger.operators :refer [$in $set]]))
+
+(def db (mg/get-db (mg/connect) "planets-api"))
+
+(defn create-db
+  "Create index in and unique keys in name on planet doc."
+  []
+  (mc/create-index db "planets" ["name"] {:unique true}))
+
+(defn destroy-db
+  "Destroy database."
+  []
+  (drop-db db))
+    
+(defn clean-db
+  "Remove all documents from database."
+  []
+  (mc/remove db "planets"))
+
+(defn insert-planet
+  "Insert a planet in db."
+  [planet]
+  (mc/insert db "planets" planet))
+
+(defn get-all-planets
+  "Get all planets from db."
+  []
+  (mc/find-maps db "planets"))
