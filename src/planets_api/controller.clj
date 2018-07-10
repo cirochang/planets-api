9e26f884b16f20c57fe25539b11a49e5720eb52c
Ciro Chang <ciro.chang@tvglobo.com.br>
2018-07-10 01:11:24 -0300

    add routes create planets and get list planets

---
+++ src/planets_api/controller.clj
@@ -0,0 +1,40 @@
+(ns planets-api.controller
+    (:require [ring.util.response :refer [response status]]
+              [schema.core :refer [validate]]
+              [planets-api.schema :as schema]
+              [planets-api.database :as database]
+              [clj-http.client :as client]
+              [cheshire.core :refer [parse-string]]))
+
+(defn- get-total-films-by-planet
+  "Make a request to https://swapi.co/api and get the number of films of planet"
+  [planet]
+  (let [planet-name (:name planet)
+        url (str "https://swapi.co/api/planets?format=json&search=" planet-name)
+        response (client/get url)
+        body (parse-string (get-in response [:body]))
+        results (get-in body ["results"])
+        planet (first results)
+        films (get-in planet ["films"])]
+    (count films)))
+
+(defn create-planet
+  "Add a planet in the database"
+  [planet]
+  (try
+    (do
+      (validate schema/planet planet)
+      (let [num-films (get-total-films-by-planet planet)
+            planet-with-films (merge planet {:num_films num-films})] 
+        (database/insert-planet planet-with-films))
+      (response "ok"))
+    (catch com.mongodb.DuplicateKeyException e
+      (status (response "This job already exists in database") 409))
+    (catch java.lang.RuntimeException e
+      (status (response (str (.getMessage e))) 400))))
+
+(defn get-list-planets
+  "Return a list of all planets."
+  []
+  (let [planets (database/get-all-planets)]
+    (response planets)))
--- src/planets_api/routes.clj
+++ src/planets_api/routes.clj
@@ -1,6 +1,9 @@
 (ns planets-api.routes
     (:require [compojure.route :as route]
-              [compojure.core :refer [defroutes]]))
+              [compojure.core :refer [defroutes POST GET]]
+              [planets-api.controller :as controller]))
 
 (defroutes app-routes
+    (POST "/planets" {body :body} (controller/create-planet body))
+    (GET "/planets" [] (controller/get-list-planets))
     (route/not-found "Not Found"))
